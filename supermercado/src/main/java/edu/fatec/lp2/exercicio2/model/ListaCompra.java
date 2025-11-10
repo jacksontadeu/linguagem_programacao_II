b5a89f3cfff3f46186fdd3b1b8b6cb2901da36c2
Jackson Moraes <jacksontadeu@hotmail.com>
2025-11-10 10:06:35 -0300

    :recycle: refactor: Refatoracao do ListaCompra

--- supermercado/src/main/java/edu/fatec/lp2/exercicio2/model/ListaCompra.java
+++ supermercado/src/main/java/edu/fatec/lp2/exercicio2/model/ListaCompra.java
@@ -1,19 +1,22 @@
 package edu.fatec.lp2.exercicio2.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ListaCompra implements Calculavel {
 
     private Integer qtdeMax;
-    List<ItemCompra> itensCompra ;
+    Set<ItemCompra> itensCompra ;
 
-    public ListaCompra(List<ItemCompra> itensCompra)  {
-        this.itensCompra = new ArrayList<>();
+    public ListaCompra(Integer qtdeMax)  {
+        this.qtdeMax = qtdeMax;
+        this.itensCompra = new HashSet<>();
     }
 
     public void incluirItem(ItemCompra item){
-        if(itensCompra.toArray().length <= this.qtdeMax){
+        if(itensCompra.size() <= this.qtdeMax){
             itensCompra.add(item);
         }else{
             System.out.println("Quantidade máxima da lista excedida");
@@ -23,7 +26,15 @@ public class ListaCompra implements Calculavel {
 
     @Override
     public double calcularPreco() {
-
-        return 0;
+        double valorTotal = 0;
+        for(ItemCompra item : itensCompra){
+            valorTotal = valorTotal + (item.getQuantidade() * item.getProduto().getPreco());
+        }
+        return valorTotal;
+    }
+    public void listar(){
+        for(var item : this.itensCompra){
+            System.out.println(item);
+        }
     }
 }
